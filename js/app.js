47b2d1e6555b1304732dc20a63c7df63d2beb42e
byeolee1221 <zicsso1221@gmail.com>
2024-05-31 20:40:25 +0900

    챌린지 이미지 안나오는 문제 수정

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const background = ["beach.jpg", "dawn.jpg", "leaves.jpg"];
 const bgRandom = background[Math.floor(Math.random() * background.length)];
 
 const onBgChange = () => {
-  document.body.style.background = `url("/img/${bgRandom}") no-repeat center/cover`;
+  document.body.style.background = `url("img/${bgRandom}") no-repeat center/cover`;
 }
 
 document.addEventListener("DOMContentLoaded", onBgChange);
